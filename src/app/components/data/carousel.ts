823dbbb7e419f6b518083cf1e527ff5c38b19ab6
nhan_sgtech <nhnhan.sgt@gmail.com>
2023-12-15 11:37:55 +0700

    #16 Add scrolling, refactor carousel

---
+++ src/app/components/data/carousel.ts
@@ -0,0 +1,24 @@
+type CarouseType = {
+    img_path: string,
+    alt: string,
+    subtitle: string,
+    title: string,
+    desc: string,
+}[];
+
+export const carouselData: CarouseType = [
+    {
+        img_path: '/img/carousel/sport_s1.webp',
+        alt: 'slider',
+        subtitle: 'SUMMER ESSENTIALS',
+        title: 'FITNESS HOUSE',
+        desc: 'Update your wardrobe with distinctive fashion style.',
+    },
+    {
+        img_path: '/img/carousel/sport_s3.webp',
+        alt: 'slider',
+        subtitle: 'WINTER MELTING',
+        title: 'MIX CLOTHES',
+        desc: 'Update your wardrobe with distinctive fashion style.',
+    }
+];
